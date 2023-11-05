7899aadea53314717c3751e20941c9ad73faf6f4
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-05 17:08:31 -0600

    1

--- registro.php
+++ registro.php
@@ -1,5 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:8080");
+header("Access-Control-Allow-Origin: https://witty-pebble-03ffd1910.4.azurestaticapps.net");
+
 header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
