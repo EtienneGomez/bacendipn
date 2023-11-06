24e6adf57eaf1dbd6758f47d7c7473da312ba3e9
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-05 23:53:01 -0600

    Update BackEnd.php

--- BackEnd.php
+++ BackEnd.php
@@ -7,8 +7,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-W
 try {
     $conn = new PDO("sqlsrv:server = tcp:servidorpruebaipn1.database.windows.net,1433; Database = base1", "servidorpruebaipn1", "Etienne098");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
@@ -17,61 +16,40 @@ $id = $_POST['id'] ?? null;
 $email = $_POST['email'] ?? null;
 $password = $_POST['password'] ?? null;
 
-try {
-    if ($email !== null && $password !== null) {
-        // Utiliza sentencias preparadas para evitar inyección SQL
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
-        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($email !== null && $password !== null) {
+    // Consulta para verificar las credenciales de inicio de sesión
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
+    $result = $conn->query($sql);
 
-        if ($user && password_verify($password, $user['password'])) {
-            // Inicio de sesión exitoso
-            unset($user['password']); // Elimina la contraseña por seguridad
-            $response = [
-                'message' => 'Inicio de sesión exitoso',
-                'id' => $user['id'],
-                'email' => $user['email'],
-                // No incluir 'password' aquí
-                'nombre' => $user['nombre'],
-                'apellidos' => $user['apellidos'],
-                'boleta' => $user['boleta'],
-                'telefono' => $user['telefono'],
-                'escuela' => $user['escuela'],
-                'plan_relacion' => $user['plan_relacion'],
-                'descripcion' => $user['descripcion'],
-                'imagen' => $user['imagen']
-            ];
-            echo json_encode($response);
-        } else {
-            // Credenciales inválidas
-            http_response_code(401);
-            echo json_encode(['message' => 'Credenciales inválidas']);
-        }
-    } elseif ($id !== null) {
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($user) {
-            unset($user['password']); // Elimina la contraseña por seguridad
-            echo json_encode(['message' => 'Usuario encontrado'] + $user);
-        } else {
-            http_response_code(404);
-            echo json_encode(['message' => 'Usuario no encontrado']);
-        }
+    if ($result->rowCount() > 0) {
+        // Inicio de sesión exitoso
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        unset($row['password']); // Es una buena práctica no enviar la contraseña, incluso si no te preocupa la seguridad
+        echo json_encode(['message' => 'Inicio de sesión exitoso'] + $row);
     } else {
-        throw new Exception('No se proporcionaron datos suficientes para la consulta.');
+        // Credenciales inválidas
+        http_response_code(401);
+        echo json_encode(['message' => 'Credenciales inválidas']);
     }
-} catch (PDOException $e) {
-    error_log("Database error: " . $e->getMessage());
-    http_response_code(500);
-    echo json_encode(['message' => 'Error de conexión a la base de datos']);
-} catch (Exception $e) {
-    error_log("Error: " . $e->getMessage());
+} elseif ($id !== null) {
+    // Consulta por ID
+    $sql = "SELECT * FROM usuarios WHERE id = '$id'";
+    $result = $conn->query($sql);
+
+    if ($result->rowCount() > 0) {
+        // Usuario encontrado
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        unset($row['password']); // Es una buena práctica no enviar la contraseña, incluso si no te preocupa la seguridad
+        echo json_encode(['message' => 'Usuario encontrado'] + $row);
+    } else {
+        // Usuario no encontrado
+        http_response_code(404);
+        echo json_encode(['message' => 'Usuario no encontrado']);
+    }
+} else {
+    // No se proporcionaron datos suficientes para la consulta
     http_response_code(400);
-    echo json_encode(['message' => $e->getMessage()]);
+    echo json_encode(['message' => 'Datos insuficientes']);
 }
 
 $conn = null;
