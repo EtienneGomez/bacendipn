2a0ea6afe3f37b9419fe35de36703ffeda2fe1fb
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-06 02:12:14 -0600

    Update BackEnd.php

--- BackEnd.php
+++ BackEnd.php
@@ -1,7 +1,4 @@
 <?php
-
-ini_set('log_errors', 'On');
-ini_set('error_log', 'php-errors.log');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
