75b98f16cff68145ab5ebc10a07acd06cf214774
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-05 23:10:12 -0600

    Update BackEnd.php

--- BackEnd.php
+++ BackEnd.php
@@ -7,44 +7,40 @@ try {
     $conn = new PDO("sqlsrv:server = tcp:servidorpruebaipn1.database.windows.net,1433; Database = base1", "servidorpruebaipn1", "Etienne098");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
     $id = $_POST['id'] ?? null;
     $email = $_POST['email'] ?? null;
     $password = $_POST['password'] ?? null;
 
     if ($email && $password) {
-        // Utiliza sentencias preparadas para evitar inyección SQL
-        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email AND password = :password");
-        $stmt->execute(['email' => $email, 'password' => $password]);
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($user && password_verify($password, $user['password'])) {
+            // Inicio de sesión exitoso
+            unset($user['password']); // Elimina la contraseña por seguridad
+            echo json_encode([
+                'message' => 'Inicio de sesión exitoso',
+            ] + $user);
+        } else {
+            // Credenciales inválidas
+            http_response_code(401);
+            echo json_encode(['message' => 'Credenciales inválidas']);
+        }
     } elseif ($id) {
         $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
         $stmt->execute(['id' => $id]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($user) {
+            unset($user['password']); // Elimina la contraseña por seguridad
+            echo json_encode([
+                'message' => 'Usuario encontrado',
+            ] + $user);
+        } else {
+            http_response_code(404);
+            echo json_encode(['message' => 'Usuario no encontrado']);
+        }
     } else {
         throw new Exception('No se proporcionaron datos suficientes para la consulta.');
-    }
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user) {
-        // Inicio de sesión exitoso
-        echo json_encode([
-            'message' => 'Inicio de sesión exitoso',
-            'id' => $row['id'],
-            'email' => $row['email'],
-            'password' => $row['password'],
-            'nombre' => $row['nombre'],
-            'apellidos' => $row['apellidos'],
-            'boleta' => $row['boleta'],
-            'telefono' => $row['telefono'],
-            'escuela' => $row['escuela'],
-            'plan_relacion' => $row['plan_relacion'],
-            'descripcion' => $row['descripcion'],
-            'imagen' => $row['imagen']
-        ]);
-    } else {
-        // Credenciales inválidas
-        http_response_code(401);
-        echo json_encode(['message' => 'Credenciales inválidas']);
     }
 } catch (PDOException $e) {
     http_response_code(500);
