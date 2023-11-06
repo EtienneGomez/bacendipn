ce101ad1d143367d13676adcb400ee613e79bf95
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-06 00:51:29 -0600

    Update BackEnd.php

--- BackEnd.php
+++ BackEnd.php
@@ -12,39 +12,24 @@ try {
     die(print_r($e));
 }
 
-$id = $_POST['id'] ?? null;
 $email = $_POST['email'] ?? null;
 $password = $_POST['password'] ?? null;
 
 if ($email !== null && $password !== null) {
     // Consulta para verificar las credenciales de inicio de sesión
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
-    $result = $conn->query($sql);
+    $sql = "SELECT * FROM usuarios WHERE email = ? AND password = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([$email, $password]);
 
-    if ($result->rowCount() > 0) {
+    if ($stmt->rowCount() > 0) {
         // Inicio de sesión exitoso
-        $row = $result->fetch(PDO::FETCH_ASSOC);
-        unset($row['password']); // Es una buena práctica no enviar la contraseña, incluso si no te preocupa la seguridad
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        unset($row['password']); // Eliminar la contraseña por seguridad
         echo json_encode(['message' => 'Inicio de sesión exitoso'] + $row);
     } else {
         // Credenciales inválidas
         http_response_code(401);
         echo json_encode(['message' => 'Credenciales inválidas']);
-    }
-} elseif ($id !== null) {
-    // Consulta por ID
-    $sql = "SELECT * FROM usuarios WHERE id = '$id'";
-    $result = $conn->query($sql);
-
-    if ($result->rowCount() > 0) {
-        // Usuario encontrado
-        $row = $result->fetch(PDO::FETCH_ASSOC);
-        unset($row['password']); // Es una buena práctica no enviar la contraseña, incluso si no te preocupa la seguridad
-        echo json_encode(['message' => 'Usuario encontrado'] + $row);
-    } else {
-        // Usuario no encontrado
-        http_response_code(404);
-        echo json_encode(['message' => 'Usuario no encontrado']);
     }
 } else {
     // No se proporcionaron datos suficientes para la consulta
