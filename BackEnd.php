3e7d514edebba2611cf43defa92432e13ca9bcec
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-05 22:36:03 -0600

    Update BackEnd.php

--- BackEnd.php
+++ BackEnd.php
@@ -3,68 +3,43 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-
-// Crear conexión
 try {
     $conn = new PDO("sqlsrv:server = tcp:servidorpruebaipn1.database.windows.net,1433; Database = base1", "servidorpruebaipn1", "Etienne098");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $id = $_POST['id'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+
+    if ($email && $password) {
+        // Utiliza sentencias preparadas para evitar inyección SQL
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email AND password = :password");
+        $stmt->execute(['email' => $email, 'password' => $password]);
+    } elseif ($id) {
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+    } else {
+        throw new Exception('No se proporcionaron datos suficientes para la consulta.');
+    }
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user) {
+        // Inicio de sesión exitoso
+        echo json_encode([
+            'message' => 'Inicio de sesión exitoso',
+            'user' => $user
+        ]);
+    } else {
+        // Credenciales inválidas
+        http_response_code(401);
+        echo json_encode(['message' => 'Credenciales inválidas']);
+    }
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['message' => 'Error de conexión a la base de datos: ' . $e->getMessage()]);
+} catch (Exception $e) {
+    http_response_code(400);
+    echo json_encode(['message' => $e->getMessage()]);
 }
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-
-$id = null;
-$password = null;
-$email = null;
-
-if(isset($_POST['id'])){
-    $id = $_POST['id'];
-}
-
-if(isset($_POST['email']) && isset($_POST['password'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-}
-
-
-if(empty($id)){
-    // Consulta para verificar las credenciales de inicio de sesión
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND password = '$password'";
-    
-}else{
-    $sql = "SELECT * FROM usuarios WHERE id = '$id'";
-}
-    $result = $conn->query($sql);
-
-if ($result->rowCount() > 0) {
-    // Inicio de sesión exitoso
-    $row = $result->fetch_assoc();
-    $response = array(
-
-        'message' => 'Inicio de sesión exitoso',
-        'id' => $row['id'],
-        'email' => $row['email'],
-        'password' => $row['password'],
-        'nombre' => $row['nombre'],
-        'apellidos' => $row['apellidos'],
-        'boleta' => $row['boleta'],
-        'telefono' => $row['telefono'],
-        'escuela' => $row['escuela'],
-        'plan_relacion' => $row['plan_relacion'],
-        'descripcion' => $row['descripcion'],
-        'imagen' => $row['imagen']
-    );
-
-    echo json_encode($response);
-} else {
-    // Credenciales inválidas
-    http_response_code(406);
-    $response = array('message' => 'Credenciales inválidas');
-    echo json_encode($response);
-}
-
-$conn = null;
-
 ?>
